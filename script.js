4c2c0514cf83838b4b4a28517545e28c1a6b8b38
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-08-18 01:28:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ replaceText(document)
 function replaceText(element) {
   if (element.hasChildNodes()) element.childNodes.forEach(replaceText)
   else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/coronavirus|covid/gi)) {
+    if (element.textContent.match(/coronavirus|covid|corona/gi)) {
       const newElement = document.createElement('span')
       newElement.innerHTML = element.textContent.replace(
         /(coronavirus|covid)/gi,
         '<span style="background-color: black; color:black">$1</span>'
       )
       element.replaceWith(newElement)
-    } 
+    }
   }
 }
